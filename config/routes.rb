49648b65328b46d959c7ec4f710e2fc69b1f55bf
Coralie Collignon <coralie.collignon@gmail.com>
2017-02-13 17:41:34 +0100

    end of setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+  resources :beds do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :delete]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
