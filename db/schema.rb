f084c4ed496869a6afdf5893d5ff11c4303723dc
Nicolas Ferrary <ferraryn@gmail.com>
2017-02-14 12:51:26 +0100

    bed creation, edit and delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213172524) do
+ActiveRecord::Schema.define(version: 20170214110645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,14 @@ ActiveRecord::Schema.define(version: 20170213172524) do
     t.integer  "price"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "city"
     t.string   "zipcode"
     t.string   "country"
+    t.boolean  "pillow"
+    t.string   "blanket_type"
+    t.boolean  "breakfast"
     t.index ["user_id"], name: "index_beds_on_user_id", using: :btree
   end
 
