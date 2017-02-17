4fb4c6c470f4e17bca145cff0f8f9345b9b7d8ca
Ludovic Woronoff <ludovicworonoff@gmail.com>
2017-02-17 11:55:33 +0100

    inprogress model

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
   namespace :my do
     resources :beds, only: [:index]
-    resources :bookings, only: [:index, :update]
+    resources :bookings, only: [:index, :update] do
+      resources :reviews, only: [:create]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
